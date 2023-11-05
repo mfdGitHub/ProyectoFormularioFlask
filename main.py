cbd4267e4fcfaf8e283141662d5227edebb155f3
MoisesDebian11 <moyinformatico@gmail.com>
2023-11-05 13:08:28 -0600

    Se agrego el método post para el formulario

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask import render_template 
+from flask import request
 
 import forms
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', method = ['GET', 'POST'])
 def index():
-    comment_form = forms.ComentForm()
+    comment_form = forms.ComentForm(request.form)
+    if request.method == 'POST':
+        print (comment_form.username.data)
+        print (comment_form.email.data)
+        print (comment_form.comment.data)
+
+
     title = "Curso Flask"
     return render_template('index.html', title = title, form = comment_form)
 
