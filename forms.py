9a9973801c960e0e04f460e162b03b9dff540fbb
MoisesDebian11 <moyinformatico@gmail.com>
2023-11-06 13:13:51 -0600

    Se agrego las validaciones de campos al formulario

--- forms.py
+++ forms.py
@@ -3,8 +3,20 @@ from wtforms import Form
 from wtforms import StringField
 from wtforms.fields import EmailField
 
+from wtforms import validators
+
 class ComentForm(Form):
-    username = StringField('username')
-    email = EmailField('Correo electronico')
+    username = StringField('username', 
+            [
+                validators.DataRequired(message = 'El username es requerido'),
+                validators.length(min=4, max=25, message="Ingrese un username valido!!!")
+                
+            ] 
+            )
+    email = EmailField('Correo electronico',
+            [
+                validators.DataRequired(message = 'El email es requerido'),
+                validators.Email(message='Ingresa un email valido!!')
+            ])
     comment = StringField('Comentario')
     
