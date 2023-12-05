89b6268961e5d269635b1f79e90157b301b88239
MoisesDebian11 <moyinformatico@gmail.com>
2023-12-05 13:16:06 -0600

    Se modifico el modelo para insertar contraseñas hash

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 import datetime 
 
 db = SQLAlchemy()
@@ -11,3 +12,12 @@ class User(db.Model):
     email = db.Column(db.String(40))
     password = db.Column(db.String(66))
     create_date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = self.__create_password(password)
+        self.email = email 
+
+    def __create_password(self, password):
+        return generate_password_hash(password)
