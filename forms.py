30b20c619de63a118b8095f93a1eec8a349ea7b9
MoisesDebian11 <moyinformatico@gmail.com>
2023-12-06 13:04:59 -0600

    Se hizo una función para validar y probar la funcionalidad method override

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms import PasswordField
 from wtforms.fields import EmailField
 from wtforms import HiddenField
 from wtforms import validators
-
+from models import User
 
 def length_honeypot(form, field):
     if len(field.data) > 0:
@@ -47,13 +47,19 @@ class CreateForm(Form):
                     validators.DataRequired(message = 'El username es requerido!!!'),
                     validators.length(min=4, max=25, message='Ingrese un username valido!!!')
                 ])
-    password = PasswordField('Password',
-                [
-                    validators.DataRequired(message='El passowrd es requerido!!!')
-                ])
     email = EmailField('Correo electronico',
             [
                 validators.DataRequired(message = 'El email es requerido'),
                 validators.Email(message='Ingresa un email valido!!'),
                 validators.length(min=4, max=50, message='Ingrese un email valido!!!')
             ])
+    password = PasswordField('Password',
+                [
+                    validators.DataRequired(message='El passowrd es requerido!!!')
+                ])
+
+    def validate_username(form, field):
+        username = field.data 
+        user = User.query.filter_by(username = username).first()
+        if user is not None:
+            raise validators.ValidationError('El username ya se encuentra registrado!!!')
