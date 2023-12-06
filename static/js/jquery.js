9c15e4e61ff099390b2e661e4ec6139040013c4c
MoisesDebian11 <moyinformatico@gmail.com>
2023-12-06 12:34:57 -0600

    Se comento la función submit

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -14,8 +14,8 @@ $(document).ready(function(){
         });
     }
 
-    $("#login-form").submit(function(event){
+    /*$("#login-form").submit(function(event){
         event.preventDefault();
         ajax_login();
-    });
+    });*/
 });
