2fda60ebb219f3a9a4a7f1497c2a6de0c480c39a
MoisesDebian11 <moyinformatico@gmail.com>
2023-12-17 03:16:29 -0600

    Se agrego la clase CommentForm en el forms para almacenar el comentario en lo que se inserta en la base de datos.

--- forms.py
+++ forms.py
@@ -11,24 +11,13 @@ def length_honeypot(form, field):
     if len(field.data) > 0:
         raise validators.ValidationError('El campo debe estar vacio.')
 
-class ComentForm(Form):
-    username = StringField('Username', 
-            [
-                validators.DataRequired(message = 'El username es requerido'),
-                validators.length(min=4, max=25, message="Ingrese un username valido!!!")
-                
-            ] 
-            )
-    email = EmailField('Correo electronico',
-            [
-                validators.DataRequired(message = 'El email es requerido'),
-                validators.Email(message='Ingresa un email valido!!')
-            ])
-    comment = StringField('Comentario')
-    honeypot = HiddenField('', 
-            [
-                length_honeypot
-            ])
+
+class CommentForm(Form):    
+    comment = StringField('Comentario',
+                [
+                    validators.DataRequired(message = 'El username es requerido!!!')
+                ])
+
 
 class LoginForm(Form):
     username = StringField('Username',
