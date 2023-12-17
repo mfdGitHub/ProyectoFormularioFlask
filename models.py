86eb3b28ee58a98a9369a9c2d2c293420f571321
MoisesDebian11 <moyinformatico@gmail.com>
2023-12-17 03:11:47 -0600

    Se agrego la clase Comment en el modulo para poder gestionar comentarios en la base de datos.

--- models.py
+++ models.py
@@ -11,7 +11,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(40))
-    password = db.Column(db.String(66))
+    password = db.Column(db.String(255))
+    comments = db.relationship('Comment')
     create_date = db.Column(db.DateTime, default=datetime.datetime.now)
 
 
@@ -25,3 +26,12 @@ class User(db.Model):
     
     def verify_password(self, password):
         return check_password_hash(self.password, password)
+    
+
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    text = db.Column(db.Text())
+    created_date = db.Column(db.DateTime, default= datetime.datetime.now)
