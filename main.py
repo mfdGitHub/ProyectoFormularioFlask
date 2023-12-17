abec726093e3502ef2da214db81e543488f9c795
MoisesDebian11 <moyinformatico@gmail.com>
2023-12-17 03:15:22 -0600

    Se agrego la funcion comment para validar la sesion del usuario y poder agregar el comentario que el usuario escribio

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from flask import redirect
 from config import DevelopmentConfig
 from models import db 
 from models import User
+from models import Comment 
 
 from flask_wtf import CSRFProtect
 import forms
@@ -20,33 +21,25 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect()
 
-@app.errorhandler(401)
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template('404.html'),404
 
 @app.before_request 
 def before_request():
-    if 'username' not in session and request.endpoint in ['comment']:
-        return redirect(url_for('login'))
-    elif 'username' in session and request.endpoint in ['login','create']:
-        return redirect(url_for('index'))
-
-
-
-@app.route('/', methods = ['GET', 'POST'])
-def index():
-    contact_form = forms.ComentForm(request.form)
-    if 'username' in session:
-        username = session['username']
-        print(username)
-        
-    title = "Index"
-    return render_template('index.html', title = title, form = contact_form)
+    pass
 
 @app.after_request
 def after_request(response):
     return response 
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+        
+    title = "Index"
+    return render_template('index.html', title = title)
 
 @app.route('/logout')
 def logout():
@@ -67,6 +60,7 @@ def login():
             success_message = 'Bienvenido {}'.format(username)
             flash(success_message)
             session['username'] = username
+            session['user_id'] = user.id 
             return redirect(url_for('index'))
         else:
             error_message = 'Usuario o password no validos!!!'
@@ -102,7 +96,19 @@ def cookie():
 
 @app.route('/comment', methods = ['GET', 'POST'])
 def comment():
-    pass
+    comment_form = forms.CommentForm(request.form)
+    if request.method == 'POST' and comment_form.validate():
+        user_id = session['user_id']
+        comment = Comment(user_id = user_id, text = comment_form.comment.data)
+
+        db.session.add(comment)
+        db.session.commit()
+        success_message = 'Nuevo comentario creado!!!'
+        flash(success_message)
+        
+    title = "Curso Flask"
+    return render_template('comment.html', title = title, form = comment_form)
+
 @app.route('/ajax-login', methods = ['POST'])
 def ajax_login():
     print(request.form)
