96ba1c4b5e7ae0c07548f7769c96b4159a9bbfe3
MoisesDebian11 <moyinformatico@gmail.com>
2023-12-22 12:20:48 -0600

    Se agregó la configuración para la gestión de correo electronico.

--- config.py
+++ config.py
@@ -2,6 +2,13 @@ import os
 
 class Config(object):
     SECRET_KEY = 'my_secret_key'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_SSL = False 
+    MAIL_USE_TLS = True 
+    MAIL_USERNAME = 'moyinformatico@gmail.com'
+    MAIL_PASSWORD = os.environ.get('PASSWORD_EMAIL_PRUEBA')
+
 
 class DevelopmentConfig(Config):
     DEBUG = True 
